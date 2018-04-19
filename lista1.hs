6dae5c6c1404512bf9df0272c717fa27d829bdf3
bvl0 <38329538+bvl0@users.noreply.github.com>
2018-04-19 14:43:18 -0300

    Add files via upload

---
+++ lista1.hs
@@ -0,0 +1,50 @@
+--b 
+arear a b = a*b
+
+--c
+areac  a =pi*(a*a)
+
+--d
+dist a b = b-a
+
+--e
+
+delta a b c = b^2 -4*a*c
+
+raiz1 a b c = ((-b + (sqrt (delta a b c))) / (2 * a))
+
+raiz2 a b c = ((-b - (sqrt (delta a b c))) / (2 * a)) 
+--g
+tf a =  a * 9 / 5 + 32
+
+--h
+
+ga v = (v *((1+0.005)^12))  -v
+
+--i
+
+areac2 a = (areac b) 
+			where b = a*2
+
+--k
+
+
+
+
+cord x y =  if x>1
+				then if y>1
+						then "primeiro quadrante"
+						else "fon"
+				
+			else if x<1
+				then if y<1
+						then "terceiro quadrante"
+						else "fon"
+			
+-- i
+intervalo a b x = if a<= b
+						then if x >=  a && x <= b
+ 						then "esta no intervalo"
+						else "nao esta no intervalo"
+				else "a maior que b"
+
